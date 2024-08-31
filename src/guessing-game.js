da95d16ee3bd21f0edd0735198b49e6d9642b764
nas233 <nas233@gmail.com>
2024-08-31 20:41:13 +0400

    feat: update guessing game js

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,33 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.currentG = null;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.currentG = Math.round((this.min + this.max) / 2);
+        if (this.max - this.min <= 1) {
+            if (this.currentG < this.max){
+                return this.max;
+            } else if (this.currentG > this.min) {
+                return this.min;
+            }
+        }
+        return this.currentG;
     }
 
     lower() {
-
+        this.max = this.currentG;
     }
 
     greater() {
-
+        this.min = this.currentG;
     }
 }
 
